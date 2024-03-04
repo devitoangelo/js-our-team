018ad1a7c4dc2bd253703c38fd435628e91de040
devitoangelo <angelodevito4@gmail.com>
2024-03-04 18:04:17 +0100

    fix bous card

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,37 +2,37 @@ const team = [
     {
         name: 'Wayne Barnett',
         role: 'Founder & CEO',
-        image: '/assets/img/wayne-barnett-founder-ceo.jpg',
+        image: '/js-our-team/assets/img/wayne-barnett-founder-ceo.jpg',
     },
 
     {
         name: 'Angela Caroll',
         role: 'Chief Editor',
-        image: '/assets/img/angela-caroll-chief-editor.jpg',
+        image: '/js-our-team/assets/img/angela-caroll-chief-editor.jpg',
     },
 
     {
         name: 'Walter Gordon',
         role: 'Office Manager',
-        image: '/assets/img/walter-gordon-office-manager.jpg',
+        image: '/js-our-team/assets/img/walter-gordon-office-manager.jpg',
     },
 
     {
         name: 'Angela Lopez',
         role: 'Social Media Manager',
-        image: '/assets/img/angela-lopez-social-media-manager.jpg',
+        image: '/js-our-team/assets/img/angela-lopez-social-media-manager.jpg',
     },
 
     {
         name: 'Scott Estrada',
         role: 'Developer',
-        image: '/assets/img/scott-estrada-developer.jpg',
+        image: '/js-our-team/assets/img/scott-estrada-developer.jpg',
     },
 
     {
         name: 'Barbara Ramos',
         role: 'Graphic Designer',
-        image: '/assets/img/barbara-ramos-graphic-designer.jpg',
+        image: '/js-our-team/assets/img/barbara-ramos-graphic-designer.jpg',
     },
 
 ];
