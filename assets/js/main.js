b1047e3ea5b4abb9d310abc7ab3741ab823eeafb
devitoangelo <angelodevito4@gmail.com>
2024-03-04 17:01:03 +0100

    values-dom Milestrone 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,16 +39,34 @@ const team = [
 //MILESTONE 1:
 // Stamapre su console le informazioni di nome ruole e la stringa della foto 
 
-for (let i = 0; i < team.length; i++) {
-    const teams = team[i];
-    console.log(teams);
+// for (let i = 0; i < team.length; i++) {
+//     const teams = team[i];
+//     console.log(teams);
 
-}
+// }
+
+
 
 // MILESTONE 2:
 // Stampare a schermo tramite un ciclo for tutte le informazioni dell'oggetto
 
 
+// recupero l'elemento della dom
+let container = document.getElementById(`container`)
+
+// ciclo all'interno della mia array
+for (let i = 0; i < team.length; i++) {
+    const teams = team[i];
+    console.log(teams);
+
+    // ad ogni iterazione creao il markup che mi serve 
+    const marKupName = `<div class="name">${teams.name}</div>`
+    const markupRole = `<div class="role">${teams.role}</div>`
+    const markupImage = `<div class="img">${teams.image}</div>`
+    container.innerHTML += marKupName
+    container.innerHTML += markupRole
+    container.innerHTML += markupImage
 
 
+}
 
--- readme.md
+++ readme.md
@@ -10,12 +10,23 @@
 
 
 # Steps
-- stamapre su console le informazioni di nome ruole e la stringa della foto 
-- stampare a schermo tramite un ciclo for tutte le informazioni dell'oggetto
+- 1.0 stamapre su console le informazioni di nome ruole e la stringa della foto 
+    - ciclo all'interno dell'array
+    - stampo i valori in console
+- 2.0 stampare a schermo tramite un ciclo for tutte le informazioni dell'oggetto
+   - recupero l'elemento dalla dom 
+   - faccio un ciclo 
+   - faccio un markup per ogni iterazione necessaria
 - trasformare la stringa foto in un'immagine effettiva
 - formare una card per ogni persona del team
 
 
 # Tools
-- 1 consol.log
-- 2 ciclo for
+- 1 
+    - ciclo for
+    - console-log
+- 2 
+    - getelementbyid
+    - ciclo for
+    - markup
+- 3    
